de62c9eea5c448bdde7dd89d90ac824e7a51ab0c
avalojandro <avalojandro@gmail.com>
2023-02-20 16:40:25 -0600

    :memo: language

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
     app: {
       head: {
+        htmlAttrs: {
+          lang: 'en'
+        },
         charset: 'utf-8',
         title: 'cattocss',
         meta: [
